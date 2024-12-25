fc550ffc453e53027fade3e196f0390488a8c341
Mickael Riss <mickael.riss@plusgrade.com>
2024-12-25 17:49:54 +0100

    respond with 200

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
 import socket  # noqa: F401
 
+def handle_request(client_socket):
+    # Read the request
+    request = client_socket.recv(1024)
+    print('Client request: ', request)
+    # Send a response
+    client_socket.sendall(b"HTTP/1.1 200 OK\r\n")
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Starting server...")
+    
+    while True:
+        #Wait for a connection
+        print("Waiting for connection...")
+        server_socket.listen()
+        client_socket, address = server_socket.accept()
+        print(f"Connection from {address}")
+        handle_request(client_socket)
+    
+    server_socket.close()
+    print("Server closed.")
 
 
 if __name__ == "__main__":
